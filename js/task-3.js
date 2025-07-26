9ed8c130729f91faf8b45cd70d7de09b6d2dc656
Alina Smalyus <smaliusalin@gmail.com>
2025-07-26 12:25:16 -0400

    add js file

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,20 @@
-// ===== Ширина елемента =====
+// ===== Фільтрація масиву чисел =====
 
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    const totalWidth = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
-    return totalWidth;
+function filterArray(numbers, value) {
+    const result = [];
+
+    for (const number of numbers) {
+    if (number > value) {
+        result.push(number);
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px"));
+    return result;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
